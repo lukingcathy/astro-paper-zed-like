7eee7e844e9c2a7231876d71c608453e503f66b3
lukingcathy <cathyluking@foxmail.com>
2025-07-07 13:09:41 +0800

    feat: allow configuration of whether to display share links

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,7 @@ export const SITE = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
+  showShareLinks: false,
   editPost: {
     enabled: true,
     text: "Suggest Changes",
